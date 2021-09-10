af64f00586db7b1ca7cb3e5683dc4a312daa5055
vladmsnk <moiseenkov-v@mail.ru>
2021-09-10 13:01:58 +0300

    implement Serializable interface for Street

--- src/main/java/serializations/SerOfStreet.java
+++ src/main/java/serializations/SerOfStreet.java
@@ -1,4 +1,34 @@
 package serializations;
 
-public class SerOfStreet {
+
+import entities.Street;
+
+import java.util.ArrayList;
+
+
+public class SerOfStreet implements Serializable<Street> {
+    public String objToJson(Street obj) {
+        SerOfHome serOfHome = new SerOfHome();
+        String homes = "[" + serOfHome.ListOfObjToJson(obj.getHomes()) + "]";
+        return "{" +
+                "\"streetName\":" + '"' + obj.getStreetName() +
+                "\"homes\":" + homes +
+                "}";
+    }
+
+    public String ListOfObjToJson(ArrayList<Street> listOfObj) {
+        if (listOfObj.size() == 0) {
+            return "";
+        }
+        String jsonString = (listOfObj.size() > 1) ? "{" : "";
+        for (Street street : listOfObj) {
+            jsonString = jsonString.concat(objToJson(street));
+            jsonString += ", ";
+        }
+        jsonString = jsonString.substring(0, jsonString.length() - 2);
+        if (listOfObj.size() > 1) {
+            jsonString += "}";
+        }
+        return jsonString;
+    }
 }
