92584910241b3e3f558dde14c4958b20eff8ceec
vladmsnk <moiseenkov-v@mail.ru>
2021-09-11 21:04:12 +0300

    Desirializable interface for pet is implemented

--- src/main/java/deserializations/DesOfPet.java
+++ src/main/java/deserializations/DesOfPet.java
@@ -7,6 +7,9 @@ import java.util.List;
 
 public class DesOfPet implements Deserializable<Pet>{
     public Pet FromJsonToObj(String jsonString) {
+        if (jsonString.isEmpty()) {
+            return null;
+        }
         String name;
         String lastName;
         int tmp1 = jsonString.indexOf(":");
@@ -21,20 +24,20 @@ public class DesOfPet implements Deserializable<Pet>{
     }
 //    {"petName":"Bob", "petType":"Cat"}, {"petName":"Bob", "petType":"Cat"}
     public ArrayList<Pet> FromJsonToList(String jsonString) {
+        if (jsonString.isEmpty()) {
+            return null;
+        }
         ArrayList<Pet> pets = new ArrayList<>();
         jsonString = jsonString.substring(1, jsonString.length() - 1);
         while (!jsonString.isEmpty()) {
-            String name;
-            String lastName;
-            int tmp1 = jsonString.indexOf(":");
-            int tmp2 = jsonString.indexOf(",");
-            name = jsonString.substring(tmp1 + 2, tmp2 - 1);
-            jsonString = jsonString.substring(tmp2 + 1);
-            tmp1 = jsonString.indexOf(":");
-            tmp2 = jsonString.indexOf("}");
-            lastName = jsonString.substring(tmp1 + 2, tmp2 - 1);
-            jsonString = jsonString.substring(tmp2);
-            pets.add(new Pet(name, lastName));
+           int goal = jsonString.indexOf("}") + 1;
+           pets.add(FromJsonToObj(jsonString.substring(0 , goal)));
+           if (goal == jsonString.length()) {
+               jsonString = "";
+           }
+           else {
+               jsonString = jsonString.substring(goal + 3);
+           }
         }
         return pets;
     }
