c7a66f051046f74dec22499810728ce04c5ae45d
vladmsnk <moiseenkov-v@mail.ru>
2021-09-12 20:17:43 +0300

    needed functions

--- src/main/java/tools/Tools.java
+++ src/main/java/tools/Tools.java
@@ -1,2 +1,10 @@
-package tools;public class Tools {
+package tools;
+
+public class Tools {
+    //https://ru.stackoverflow.com
+
+    public static int countEntry(String initialStr, String substr) {
+        return (initialStr.length() - initialStr.replace(substr, "").length()) / substr.length();
+    }
+
 }
