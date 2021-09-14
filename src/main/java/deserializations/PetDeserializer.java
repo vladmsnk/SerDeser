4530cfac67223c4799f6c53f93fe883c4000ca76
vladmsnk <moiseenkov-v@mail.ru>
2021-09-14 15:46:05 +0300

    change names of variables, remove constructor calling and add Builder

--- src/main/java/deserializations/PetDeserializer.java
+++ src/main/java/deserializations/PetDeserializer.java
@@ -4,47 +4,40 @@ import entities.Pet;
 import restrictions.Animals;
 
 import java.util.ArrayList;
-import java.util.List;
 
 
-public class DesOfPet implements Deserializable<Pet>{
+
+public class PetDeserializer implements Deserializer<Pet> {
     public Pet FromJsonToObj(String jsonStringOfPet) {
         if (jsonStringOfPet.isEmpty()) {
             return null;
         }
-        int tmp1 = jsonStringOfPet.indexOf(":");
-        int tmp2 = jsonStringOfPet.indexOf(",");
-        String name = jsonStringOfPet.substring(tmp1 + 2, tmp2 - 1);
-        jsonStringOfPet = jsonStringOfPet.substring(tmp2 + 1, jsonStringOfPet.length());
-        tmp1 = jsonStringOfPet.indexOf(":");
-        tmp2 = jsonStringOfPet.indexOf("}");
-        String lastName = jsonStringOfPet.substring(tmp1 + 2, tmp2 - 1);
-        jsonStringOfPet = jsonStringOfPet.substring(tmp2 + 1, jsonStringOfPet.length());
-        return new Pet(name, Animals.BIRD);
+        int index1 = jsonStringOfPet.indexOf(":");
+        int index2  = jsonStringOfPet.indexOf(",");
+        String petName = jsonStringOfPet.substring(index1 + 2, index2 - 1);
+        jsonStringOfPet = jsonStringOfPet.substring(index2 + 1, jsonStringOfPet.length());
+        index1 = jsonStringOfPet.indexOf(":");
+        index2 = jsonStringOfPet.indexOf("}");
+        String animalType = jsonStringOfPet.substring(index1 + 2, index2 - 1);
+        jsonStringOfPet = jsonStringOfPet.substring(index2 + 1);
+        return new Pet.Builder().withPetName(petName).withAnimalType(Animals.valueOf(animalType)).build();
     }
 //  example  {{"petName":"Bob", "petType":"Cat"}, {"petName":"Bob", "petType":"Cat"}}
-    public ArrayList<Pet> FromJsonToList(String jsonString) {
-        if (jsonString.isEmpty()) {
+    public ArrayList<Pet> FromJsonToList(String jsonStringOfPets) {
+        if (jsonStringOfPets.isEmpty()) {
             return null;
         }
-
         ArrayList<Pet> pets = new ArrayList<>();
         //delete external { and }
-        jsonString = jsonString.substring(1, jsonString.length() - 1);
-        while (!jsonString.isEmpty()) {
-            //goal is the index of the } which closes json substring
-           int goal = jsonString.indexOf("}") + 1;
-           //implement parcing for one pet, we have there the thing like this {"petName":"Bob", "petType":"Cat"}
-            // so it is simple to implement desirialization for one pet
-            //after it we add the object to the array
-           pets.add(FromJsonToObj(jsonString.substring(0 , goal)));
-           if (goal == jsonString.length()) {
-               // if we at the end of the thring
-               jsonString = "";
+        jsonStringOfPets = jsonStringOfPets.substring(1, jsonStringOfPets.length() - 1);
+        while (!jsonStringOfPets.isEmpty()) {
+           int goal = jsonStringOfPets.indexOf("}") + 1;
+           pets.add(FromJsonToObj(jsonStringOfPets.substring(0 , goal)));
+           if (goal == jsonStringOfPets.length()) {
+               jsonStringOfPets = "";
            }
            else {
-               // we skip coma
-               jsonString = jsonString.substring(goal + 3);
+               jsonStringOfPets = jsonStringOfPets.substring(goal + 3);
            }
         }
         return pets;
