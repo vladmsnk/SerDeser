2d614767c6e185f8bd29ecf96fc1c23d7a8dd149
vladmsnk <moiseenkov-v@mail.ru>
2021-09-14 16:58:37 +0300

    Add Home Builder

--- src/main/java/entities/Home.java
+++ src/main/java/entities/Home.java
@@ -3,23 +3,40 @@ package entities;
 import java.util.ArrayList;
 
 public class Home {
-    public Home(int homeNumber) {
+
+    private final int homeNumber;
+    private final ArrayList<Room> rooms;
+
+    public Home(int homeNumber, ArrayList<Room> rooms) {
         this.homeNumber = homeNumber;
-    }
-
-    private int homeNumber;
-    private ArrayList<Room> roomes = new ArrayList<>();
-
-    public void addRoom(Room room) {
-        roomes.add(room);
+        this.rooms = rooms;
     }
 
     public String getHomeNumber() {
         return String.valueOf(homeNumber);
     }
-
-    public ArrayList<Room> getRoomes() {
-        return new ArrayList<>(roomes);
+    public ArrayList<Room> getRooms() {
+        return new ArrayList<>(rooms);
     }
+
+    public static class Builder {
+        private int homeNumber;
+        private ArrayList<Room> rooms;
+
+        public Builder withHomeNumber(int homeNumber) {
+            this.homeNumber = homeNumber;
+            return this;
+        }
+
+        public Builder withRooms(ArrayList<Room> rooms) {
+            this.rooms = rooms;
+            return this;
+        }
+
+        public Home build() {
+            return new Home(homeNumber, rooms);
+        }
+    }
+
 
 }
