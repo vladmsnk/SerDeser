1ce389d24149baf253f2ff9bca3d3ad676c4ca1a
vladmsnk <moiseenkov-v@mail.ru>
2021-09-14 17:10:51 +0300

    add Street Builder

--- src/main/java/entities/Street.java
+++ src/main/java/entities/Street.java
@@ -3,15 +3,13 @@ package entities;
 import java.util.ArrayList;
 
 public class Street {
-    public Street(String streetName) {
+
+    private final String streetName;
+    private final ArrayList<Home> homes;
+
+    public Street(String streetName, ArrayList<Home> homes) {
         this.streetName = streetName;
-    }
-
-    private ArrayList<Home> homes = new ArrayList<>();
-    private String streetName;
-
-    public void addHome(Home home) {
-        homes.add(home);
+        this.homes = homes;
     }
 
     public String getStreetName() {
@@ -20,5 +18,24 @@ public class Street {
 
     public ArrayList<Home> getHomes() {
         return new ArrayList<>(homes);
+    }
+
+    public static class Builder {
+        private String streetName;
+        private ArrayList<Home> homes;
+
+        public Builder withStreetName(String streetName) {
+            this.streetName = streetName;
+            return this;
+        }
+
+        public Builder withHomes(ArrayList<Home> homes) {
+            this.homes = homes;
+            return this;
+        }
+
+        public Street build() {
+            return new Street(streetName, homes);
+        }
     }
 }
