c3a9d8967130d0420caa57e18083e402fb2194b7
vladmsnk <moiseenkov-v@mail.ru>
2021-09-14 16:07:22 +0300

    add Person Builder

--- src/main/java/entities/Person.java
+++ src/main/java/entities/Person.java
@@ -5,39 +5,27 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Person {
-    public Person(String personName, String personLastName, int moneyCount) {
-        this.personName = personName;
-        this.personLastName = personLastName;
-        this.moneyCount = moneyCount;
-        this.petCount = 0;
-    }
-
-    private String personName;
-    private String personLastName;
-    private int moneyCount;
-    private int petCount;
+    private final String personName;
+    private final String personLastName;
+    private final int moneyCount;
     private ArrayList<Pet> personsPet = new ArrayList<Pet>();
 
-
+    public Person(String personName, String personLastName, int moneyCount, ArrayList<Pet> personsPet) {
+        this.personName = personName;
+        this.personLastName = personLastName;
+        this.personsPet = personsPet;
+        this.moneyCount = moneyCount;
+    }
     public String getPersonName() {
         return personName;
     }
-
     public String getPersonLastName() {
+
         return personLastName;
     }
 
-    public void setMoney(int moneyCount) {
-        this.moneyCount = moneyCount;
-    }
-
-    public void assignPet(Pet pet) {
-        this.petCount++;
-        this.personsPet.add(pet);
-    }
-
-    public String getPetCount() {
-        return String.valueOf(petCount);
+    public int getPetCount() {
+        return personsPet.size();
     }
     public String getMoneyCount() {
         return String.valueOf(moneyCount);
@@ -46,4 +34,35 @@ public class Person {
         return new ArrayList<>(personsPet);
     }
 
+    public static class Builder {
+        private String personName;
+        private String personLastName;
+        private int moneyCount;
+        private ArrayList<Pet> personsPets;
+
+        public Builder withPersonName(String personName) {
+            this.personName = personName;
+            return this;
+        }
+
+        public Builder withPersonLastName(String personLastName) {
+            this.personLastName = personLastName;
+            return this;
+        }
+
+        public Builder withMoneyCount(int moneyCount) {
+            this.moneyCount = moneyCount;
+            return this;
+        }
+
+        public Builder withPets(ArrayList<Pet> personsPets) {
+            this.personsPets = personsPets;
+            return this;
+        }
+
+        public Person build() {
+            return new Person(personName, personLastName, moneyCount, personsPets);
+        }
+
+    }
 }
