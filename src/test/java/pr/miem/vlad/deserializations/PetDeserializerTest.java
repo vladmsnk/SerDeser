8574ed346a335823b6958f8717c31c5968c7dac1
vladmsnk <moiseenkov-v@mail.ru>
2021-09-27 17:59:34 +0300

    pet array deserializer

--- src/test/java/pr/miem/vlad/deserializations/PetDeserializerTest.java
+++ src/test/java/pr/miem/vlad/deserializations/PetDeserializerTest.java
@@ -3,6 +3,7 @@ package pr.miem.vlad.deserializations;
 import org.junit.jupiter.api.Test;
 import pr.miem.vlad.entities.Pet;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -11,6 +12,8 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 
 public class PetDeserializerTest {
+
+    private final PetDeserializer petDeserializer = new PetDeserializer();
 
     @Test
     public void shouldParsePet() {
@@ -24,14 +27,31 @@ public class PetDeserializerTest {
         assertTrue(parsedPet.containsKey("animalType"));
         assertEquals(expectedPet, parsedPet);
     }
+    public void shouldParsePets() {
+        String jsonPets = "[{\"petName\": \"Bob\", \"animalType\": \"DOG\"}, {\"petName\": \"Bill\", \"animalType\": \"CAT\"}, {\"petName\": \"Tom\", \"animalType\": \"BIRD\"}]";
+
+    }
     @Test
     public void shouldCreatePetObject() {
         String jsonPet = "{\"petName\":\"Bill\", \"animalType\":\"DOG\"}";
-        PetDeserializer petDeserializer = new PetDeserializer();
         Pet pet = petDeserializer.fromJsonToObj(jsonPet);
         assertEquals(pet.getPetName(), "Bill");
         assertEquals(pet.getAnimalType(), "DOG");
     }
+
+    @Test
+    public void shouldCreateArrayOfPetObjects() {
+        String jsonPets = "[{\"petName\": \"Bob\", \"animalType\": \"DOG\"}, {\"petName\": \"Bill\", \"animalType\": \"CAT\"}, {\"petName\": \"Tom\", \"animalType\": \"BIRD\"}]";
+        ArrayList<Pet> pets = petDeserializer.fromJsonToList(jsonPets);
+        assertEquals(pets.size(), 3);
+        assertEquals(pets.get(0).getPetName(), "Bob");
+        assertEquals(pets.get(0).getAnimalType(), "DOG");
+        assertEquals(pets.get(1).getPetName(), "Bill");
+        assertEquals(pets.get(1).getAnimalType(), "CAT");
+        assertEquals(pets.get(2).getPetName(), "Tom");
+        assertEquals(pets.get(2).getAnimalType(), "BIRD");
+    }
+
 
 
 }
