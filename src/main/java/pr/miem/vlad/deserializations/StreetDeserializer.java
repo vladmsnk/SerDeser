dc988375511ec680747ce9164eed76f5b18559f9
vladmsnk <moiseenkov-v@mail.ru>
2021-09-29 00:23:29 +0300

    completed street deserializer

--- src/main/java/pr/miem/vlad/deserializations/StreetDeserializer.java
+++ src/main/java/pr/miem/vlad/deserializations/StreetDeserializer.java
@@ -1,52 +1,31 @@
-package deserializations;
+package pr.miem.vlad.deserializations;
 
-import entities.Home;
-import entities.Street;
-import tools.Tools;
+import pr.miem.vlad.entities.Home;
+import pr.miem.vlad.entities.Room;
+import pr.miem.vlad.entities.Street;
+import pr.miem.vlad.tools.Tools;
 
 import java.util.ArrayList;
+import java.util.Map;
 
 public class StreetDeserializer implements Deserializer<Street> {
-    public Street FromJsonToObj(String jsonStringOfStreet) {
-        if (jsonStringOfStreet == null) {
-            return null;
-        }
-        jsonStringOfStreet = jsonStringOfStreet.replaceAll("\\s+", "");
-        int index1 = jsonStringOfStreet.indexOf(":") + 2;
-        int index2 = jsonStringOfStreet.indexOf(",") - 1;
-        String streetName = jsonStringOfStreet.substring(index1, index2);
-        jsonStringOfStreet = jsonStringOfStreet.substring(index2 + 1);
-        index1 = jsonStringOfStreet.indexOf("[");
-        if (Tools.countEntry(jsonStringOfStreet, "streetName") == 0) {
-            index2 = jsonStringOfStreet.length() - 1;
-        }
-        else {
-            index2 = jsonStringOfStreet.indexOf("streetName") - 4;
-        }
+    public Street fromJsonToObj(String jsonStringOfStreet) {
+        ParseJsonObject parseJsonObject = new ParseJsonObject(jsonStringOfStreet);
+        Map<String, String> mapOfJson = parseJsonObject.getMapOfJson();
         HomeDeserializer homeDeserializer = new HomeDeserializer();
-        ArrayList<Home> homes = homeDeserializer.FromJsonToList(jsonStringOfStreet.substring(index1, index2));
-        return new Street.Builder().withStreetName(streetName).withHomes(homes).build();
+        ArrayList<Home> homes = homeDeserializer.fromJsonToList(mapOfJson.get("homes"));
+        return new Street.Builder()
+                .withStreetName(mapOfJson.get("streetName"))
+                .withHomes(homes)
+                .build();
     }
 
-    public ArrayList<Street> FromJsonToList(String jsonStringOfStreets) {
-        if (jsonStringOfStreets.isEmpty()) {
-            return null;
-        }
-        jsonStringOfStreets = jsonStringOfStreets.replaceAll("\\s+", "");
+    public ArrayList<Street> fromJsonToList(String jsonStringOfStreets) {
+        ArrayList<String> arrayOfJsonObjects = Tools.splitJsonString(jsonStringOfStreets);
         ArrayList<Street> streets = new ArrayList<>();
-        jsonStringOfStreets = jsonStringOfStreets.substring(1, jsonStringOfStreets.length() - 1);
-        while (!jsonStringOfStreets.isEmpty()) {
-            if (Tools.countEntry(jsonStringOfStreets, "streetName") == 1) {
-                streets.add(FromJsonToObj(jsonStringOfStreets));
-                jsonStringOfStreets = "";
-                break;
-            }
-            else {
-                int doubleDotIndex = jsonStringOfStreets.indexOf(":");
-                int indexOfStreetName = jsonStringOfStreets.indexOf("streetName",doubleDotIndex) - 3;
-                streets.add(FromJsonToObj(jsonStringOfStreets.substring(0, indexOfStreetName)));
-                jsonStringOfStreets = jsonStringOfStreets.substring(indexOfStreetName + 3);
-            }
+        for (String jsonObject: arrayOfJsonObjects) {
+            Street street = fromJsonToObj(jsonObject);
+            streets.add(street);
         }
         return streets;
     }
