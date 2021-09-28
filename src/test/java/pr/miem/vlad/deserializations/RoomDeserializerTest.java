982464d7c21f380219af3643a1ccbf71840a829a
vladmsnk <moiseenkov-v@mail.ru>
2021-09-29 00:13:58 +0300

    room deserializer test is completed

--- src/test/java/pr/miem/vlad/deserializations/RoomDeserializerTest.java
+++ src/test/java/pr/miem/vlad/deserializations/RoomDeserializerTest.java
@@ -29,4 +29,27 @@ public class RoomDeserializerTest {
         assertEquals(pets.get(0).getAnimalType(), "CAT");
     }
 
+    @Test
+    public void shouldCreateArrayOfRoomObjects() {
+        String jsonObject = "{\"roomNumber\": 34, \"residents\": [{\"personName\": \"Bob\", \"personLastName\": \"Ivanov\", \"moneyCount\": 12323, \"pets\": [{\"petName\": \"Musya\", \"animalType\": \"CAT\"}]}]}," +
+                " {\"roomNumber\": 35, \"residents\": [{\"personName\": \"Anna\", \"personLastName\": \"Ivanova\", \"moneyCount\": 123, \"pets\": [{\"petName\": \"Tom\", \"animalType\": \"DOG\"}]}]}";
+        ArrayList<Room> rooms = roomDeserializer.fromJsonToList(jsonObject);
+        assertEquals(Integer.parseInt(rooms.get(0).getRoomNumber()), 34);
+        assertEquals(Integer.parseInt(rooms.get(1).getRoomNumber()), 35);
+        ArrayList<Person> residents1 = rooms.get(0).getResidents();
+        ArrayList<Person> residents2 = rooms.get(1).getResidents();
+        assertEquals(residents1.get(0).getPersonName(),"Bob");
+        assertEquals(residents2.get(0).getPersonName(),"Anna");
+        assertEquals(residents1.get(0).getPersonLastName(),"Ivanov");
+        assertEquals(residents2.get(0).getPersonLastName(),"Ivanova");
+        assertEquals(Integer.parseInt(residents1.get(0).getMoneyCount()),12323);
+        assertEquals(Integer.parseInt(residents2.get(0).getMoneyCount()),123);
+        ArrayList<Pet> residentPets1 = residents1.get(0).getPersonsPet();
+        ArrayList<Pet> residentPets2 = residents2.get(0).getPersonsPet();
+        assertEquals(residentPets1.get(0).getPetName(), "Musya");
+        assertEquals(residentPets1.get(0).getAnimalType(), "CAT");
+        assertEquals(residentPets2.get(0).getPetName(), "Tom");
+        assertEquals(residentPets2.get(0).getAnimalType(), "DOG");
+    }
+
 }
