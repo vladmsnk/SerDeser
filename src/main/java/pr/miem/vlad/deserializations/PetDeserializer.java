ed092e231e919dd911a298f62899aa0c7916c5e2
vladmsnk <moiseenkov-v@mail.ru>
2021-09-28 18:57:34 +0300

    completed Pet Deserializer

--- src/main/java/pr/miem/vlad/deserializations/PetDeserializer.java
+++ src/main/java/pr/miem/vlad/deserializations/PetDeserializer.java
@@ -2,15 +2,17 @@ package pr.miem.vlad.deserializations;
 
 import pr.miem.vlad.entities.Pet;
 import pr.miem.vlad.restrictions.AnimalType;
+import pr.miem.vlad.tools.Tools;
+
+
 
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
+//[{\"petName\": \"Bob\", \"animalType\": \"DOG\"}, {\"petName\": \"Bill\", \"animalType\": \"CAT\"}]
 
 public class PetDeserializer implements Deserializer<Pet> {
-
-
 
     public Pet fromJsonToObj(String jsonString) {
         ParseJsonObject parseJsonObject = new ParseJsonObject(jsonString);
@@ -19,7 +21,12 @@ public class PetDeserializer implements Deserializer<Pet> {
     }
 
     public ArrayList<Pet> fromJsonToList(String jsonStringOfPets) {
-
-        return new ArrayList<Pet>();
+        ArrayList<String> arrayOfJsonObjects = Tools.splitJsonString(jsonStringOfPets);
+        ArrayList<Pet> pets = new ArrayList<>();
+        for (String jsonObject : arrayOfJsonObjects) {
+            Pet pet = fromJsonToObj(jsonObject);
+            pets.add(pet);
+        }
+        return pets;
     }
 }
