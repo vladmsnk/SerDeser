a9a4b7cefa0ca75e4b3084dab08e45981d25f890
vladmsnk <moiseenkov-v@mail.ru>
2021-09-28 18:57:57 +0300

    completed Person Deserializer

--- src/main/java/pr/miem/vlad/deserializations/PersonDeserializer.java
+++ src/main/java/pr/miem/vlad/deserializations/PersonDeserializer.java
@@ -2,6 +2,7 @@ package pr.miem.vlad.deserializations;
 
 import pr.miem.vlad.entities.Person;
 import pr.miem.vlad.entities.Pet;
+import pr.miem.vlad.tools.Tools;
 
 import java.util.ArrayList;
 import java.util.Map;
@@ -10,13 +11,22 @@ public class PersonDeserializer implements Deserializer<Person> {
     public Person fromJsonToObj(String jsonStringOfPerson) {
         ParseJsonObject parseJsonObject = new ParseJsonObject(jsonStringOfPerson);
         Map<String, String> mapOfJson = parseJsonObject.getMapOfJson();
-        for (String value : mapOfJson.values()) {
-            System.out.println(value);
-        }
-        return null;
+        PetDeserializer petDeserializer = new PetDeserializer();
+        ArrayList<Pet> pets = petDeserializer.fromJsonToList(mapOfJson.get("pets"));
+        return new Person.Builder()
+                .withPersonName(mapOfJson.get("personName"))
+                .withPersonLastName(mapOfJson.get("personLastName"))
+                .withMoneyCount(Integer.parseInt(mapOfJson.get("moneyCount")))
+                .withPets(pets)
+                .build();
     }
-
     public ArrayList<Person> fromJsonToList(String jsonStringOfPeople) {
-        return new ArrayList<Person>();
+        ArrayList<String> arrayOfJsonObjects = Tools.splitJsonString(jsonStringOfPeople);
+        ArrayList<Person> people = new ArrayList<>();
+        for (String jsonObject : arrayOfJsonObjects) {
+            Person person = fromJsonToObj(jsonObject);
+            people.add(person);
+        }
+        return people;
     }
 }
