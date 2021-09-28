7607f81cf5afa3b04934bf9310127db31dbd52fc
vladmsnk <moiseenkov-v@mail.ru>
2021-09-28 19:15:35 +0300

    change room Deserializer

--- src/main/java/pr/miem/vlad/deserializations/RoomDeserializer.java
+++ src/main/java/pr/miem/vlad/deserializations/RoomDeserializer.java
@@ -1,52 +1,32 @@
-package deserializations;
+package pr.miem.vlad.deserializations;
 
-import entities.Person;
-import entities.Room;
+import pr.miem.vlad.entities.Person;
+import pr.miem.vlad.entities.Room;
 import java.util.ArrayList;
-import tools.Tools;
+import java.util.Map;
+
+import pr.miem.vlad.tools.Tools;
 
 public class RoomDeserializer implements Deserializer<Room> {
 
 
-    public Room FromJsonToObj(String jsonStringOfRoom) {
-        if (jsonStringOfRoom == null) {
-            return null;
-        }
-        jsonStringOfRoom = jsonStringOfRoom.replaceAll("\\s+", "");
-        int index1 = jsonStringOfRoom.indexOf(":") + 2;
-        int index2 = jsonStringOfRoom.indexOf(",") - 1;
-        int roomNumber = Integer.parseInt(jsonStringOfRoom.substring(index1, index2));
-        jsonStringOfRoom = jsonStringOfRoom.substring(index2 + 1);
-        index1 = jsonStringOfRoom.indexOf("[");
-        if (Tools.countEntry(jsonStringOfRoom, "roomNumber") == 0) {
-            index2 = jsonStringOfRoom.length() - 1;
-        }
-        else {
-            index2 = jsonStringOfRoom.indexOf("roomNumber") - 4;
-        }
+    public Room fromJsonToObj(String jsonStringOfRoom) {
+        ParseJsonObject parseJsonObject = new ParseJsonObject(jsonStringOfRoom);
+        Map<String, String> mapOfJson = parseJsonObject.getMapOfJson();
         PersonDeserializer personDeserializer = new PersonDeserializer();
-        ArrayList<Person> residents = personDeserializer.FromJsonToList(jsonStringOfRoom.substring(index1, index2));
-        return new Room.Builder().withRoomNumber(roomNumber).withResidents(residents).build();
+        ArrayList<Person> people = personDeserializer.fromJsonToList(mapOfJson.get("people"));
+        return new Room.Builder()
+                .withRoomNumber(Integer.parseInt(mapOfJson.get("roomNumber")))
+                .withResidents(people)
+                .build();
     }
 
-    public ArrayList<Room> FromJsonToList(String jsonStringOfRooms) {
-        jsonStringOfRooms = jsonStringOfRooms.replaceAll("\\s+", "");
-        if (jsonStringOfRooms.isEmpty()) {
-            return null;
-        }
+    public ArrayList<Room> fromJsonToList(String jsonStringOfRooms) {
+        ArrayList<String> arrayOfJsonObjects = Tools.splitJsonString(jsonStringOfRooms);
         ArrayList<Room> rooms = new ArrayList<>();
-        jsonStringOfRooms = jsonStringOfRooms.substring(1, jsonStringOfRooms.length() - 1);
-        while (!jsonStringOfRooms.isEmpty()) {
-            if (Tools.countEntry(jsonStringOfRooms, "roomNumber") == 1) {
-                rooms.add(FromJsonToObj(jsonStringOfRooms));
-                break;
-            }
-            else {
-                int doubleDotIndex = jsonStringOfRooms.indexOf(":");
-                int roomNumberIndex = jsonStringOfRooms.indexOf("roomNumber",doubleDotIndex) - 3;
-                rooms.add(FromJsonToObj(jsonStringOfRooms.substring(0, roomNumberIndex)));
-                jsonStringOfRooms = jsonStringOfRooms.substring(roomNumberIndex + 2);
-            }
+        for (String jsonObject : arrayOfJsonObjects) {
+            Room room = fromJsonToObj(jsonObject);
+            rooms.add(room);
         }
         return rooms;
     }
