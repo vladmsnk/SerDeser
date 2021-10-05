ac41ae4bc74b03a923c7b78a3b1521fe8c61aa19
vladmsnk <moiseenkov-v@mail.ru>
2021-10-05 15:37:27 +0300

    add methods that create Pet Objects

--- src/test/java/pr/miem/vlad/deserializations/PetDeserializerTest.java
+++ src/test/java/pr/miem/vlad/deserializations/PetDeserializerTest.java
@@ -17,22 +17,33 @@ public class PetDeserializerTest {
     public void shouldCreatePetObject() {
         String jsonPet = "{\"name\":\"Bill\", \"animal\":\"DOG\"}";
         Pet parsedPet = petDeserializer.fromJsonToObj(jsonPet);
-        Pet expectedPet = new Pet.Builder()
-                .withName("Bill")
-                .withAnimal(Animal.valueOf("DOG"))
-                .build();
+        Pet expectedPet = createPet();
         assertEquals(expectedPet, parsedPet);
     }
 
     @Test
     public void shouldCreateArrayOfPetObjects() {
-        String jsonPets = "[{\"name\": \"Bob\", \"animal\": \"DOG\"}, {\"name\": \"Bill\", \"animal\": \"CAT\"}]";
+        String jsonPets = "[{\"name\": \"Bob\", \"animal\": \"DOG\"}, {\"name\": \"Bill\", \"animal\": \"CAT\"}, {\"name\": \"Tom\", \"animal\": \"BIRD\"}]";
         ArrayList<Pet> parsedPets = petDeserializer.fromJsonToList(jsonPets);
-        ArrayList<Pet> expectedPets = new ArrayList<>();
+        ArrayList<Pet> expectedPets = createPetList();
+        assertEquals(parsedPets, expectedPets);
+    }
+
+    private Pet createPet() {
+        return new Pet.Builder()
+                .withName("Bill")
+                .withAnimal(Animal.valueOf("DOG"))
+                .build();
+    }
+
+    private ArrayList<Pet> createPetList() {
         Pet pet1 = new Pet.Builder().withName("Bob").withAnimal(Animal.valueOf("DOG")).build();
         Pet pet2 = new Pet.Builder().withName("Bill").withAnimal(Animal.valueOf("CAT")).build();
-        expectedPets.add(pet1);
-        expectedPets.add(pet2);
-        assertEquals(parsedPets, expectedPets);
+        Pet pet3 = new Pet.Builder().withName("Tom").withAnimal(Animal.valueOf("BIRD")).build();
+        ArrayList<Pet> pets = new ArrayList<>();
+        pets.add(pet1);
+        pets.add(pet2);
+        pets.add(pet3);
+        return pets;
     }
 }
